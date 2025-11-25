5363d23a90ca27a5feec02c8d9128362820b14d1
Tristan Downey <tristan.downey@student.unsw.edu.au>
2025-11-25 13:02:42 +1100

    lighting chapter

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -6,11 +6,10 @@ layout (location = 1) in vec2 aTexCoord;
 out vec2 TexCoord;
 
 uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 viewProjection;
 
 void main() {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = viewProjection * model * vec4(aPos, 1.0);
     TexCoord = aTexCoord;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -138,34 +138,20 @@ int main(int argc, char* argv[]) {
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f), 
-        glm::vec3( 2.0f,  5.0f, -15.0f), 
-        glm::vec3(-1.5f, -2.2f, -2.5f),  
-        glm::vec3(-3.8f, -2.0f, -12.3f),  
-        glm::vec3( 2.4f, -0.4f, -3.5f),  
-        glm::vec3(-1.7f,  3.0f, -7.5f),  
-        glm::vec3( 1.3f, -2.0f, -2.5f),  
-        glm::vec3( 1.5f,  2.0f, -2.5f), 
-        glm::vec3( 1.5f,  0.2f, -1.5f), 
-        glm::vec3(-1.3f,  1.0f, -1.5f)  
-    };
-
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // texture coord attribute
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    // unbind VAO
+    glBindVertexArray(0);
 
     // load and create textures
     unsigned int texture1, texture2;
@@ -207,15 +193,41 @@ int main(int argc, char* argv[]) {
     }
     stbi_image_free(data);
 
+    unsigned int lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // using the same VBO as the cubes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0); // unbind
+
     // tell opengl for each sample to which texture unit it belongs to
     shaderProgram.use();
     shaderProgram.setInt("texture1", 0);
     shaderProgram.setInt("texture2", 1);
 
+    // bind textures on corresponding texture units
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    // define the cube positions
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 15.0f,  0.0f,  0.0f), 
+        glm::vec3( 0.0f,  15.0f,  0.0f), 
+        glm::vec3( 0.0f,  0.0f,  15.0f), 
+        glm::vec3( 0.0f,  0.0f,  -15.0f), 
+        glm::vec3( 0.0f,  -15.0f,  0.0f), 
+        glm::vec3( -15.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f)
+    };
+
     // resets lastFrame before entering render loop
     lastFrame = glfwGetTime();
     
-    while(!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window)) {
         // pre-frame time logic
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
@@ -228,40 +240,25 @@ int main(int argc, char* argv[]) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        // bind textures on corresponding texture units
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-        
         // activate shader
         shaderProgram.use();
+        shaderProgram.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        shaderProgram.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 
-        // pass projection matrix to shader
+        // generate viewProjection matrix
         glm::mat4 projection = glm::perspective(glm::radians(camera.getZoom()), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        shaderProgram.setMat4("projection", projection);
-
-        // camera/view transformation
         glm::mat4 view = camera.getViewMatrix();
-        shaderProgram.setMat4("view", view);
+
+        glm::mat4 viewProjection = projection * view;
+
+        shaderProgram.setMat4("viewProjection", viewProjection);
 
         // render boxes
         glBindVertexArray(VAO);
-        for(unsigned int i = 0; i < 10; i++) {
+        for (auto pos : cubePositions) {
             // calculate the model matrix for each object and pass to shader before drawing
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle;
-
-            if (i % 3 == 0) {
-                angle = 20.0f * i + 20.0f * (float)glfwGetTime();
-            } else {
-                angle = 20.0f * i;
-            }
-            
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), pos);
             shaderProgram.setMat4("model", model);
-
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
